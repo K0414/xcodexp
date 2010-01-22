f5d7d69052a2a5f8fcf2bc13803d7054e3659bd5
asklux <asklux@398d6864-e22c-11de-a155-4d945b6d75ec>
2010-01-22 08:07:42 +0000

    apue fig1.8

--- apue/test.c
+++ apue/test.c
@@ -1,30 +1,11 @@
 #include "apue.h"
-#include <sys/wait.h>
+#include <errno.h>
 
 int
-main(void)
+main(int argc, char *argv[])
 {
-	char	buf[MAXLINE];	/* from apue.h */
-	pid_t	pid;
-	int		status;
-
-	printf("%% ");	/* print prompt (printf requires %% to print %) */
-	while (fgets(buf, MAXLINE, stdin) != NULL) {
-		if (buf[strlen(buf) - 1] == '\n')
-			buf[strlen(buf) - 1] = 0; /* replace newline with null */
-
-		if ((pid = fork()) < 0) {
-			err_sys("fork error");
-		} else if (pid == 0) {		/* child */
-			execlp(buf, buf, (char *)0);
-			err_ret("couldn't execute: %s", buf);
-			exit(127);
-		}
-
-		/* parent */
-		if ((pid = waitpid(pid, &status, 0)) < 0)
-			err_sys("waitpid error");
-		printf("%% ");
-	}
+    fprintf(stderr, "EACCES: %s\n", strerror(EACCES));
+    errno = ENOENT;
+    perror(argv[0]);
 	exit(0);
 }
