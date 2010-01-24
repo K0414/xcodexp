40f121819f2009296983cf505c61288ef8a5fdac
asklux <asklux@398d6864-e22c-11de-a155-4d945b6d75ec>
2010-01-24 14:51:06 +0000

    apue fig3.2

--- apue/test.c
+++ apue/test.c
@@ -1,11 +1,29 @@
 #include "apue.h"
+#include <fcntl.h>
+
+char    buf1[] = "abcdefghij";
+char    buf2[] = "ABCDEFGHIJ";
 
 int
 main(void)
 {
-    if(lseek(STDIN_FILENO, 0, SEEK_CUR) == -1)
-        printf("cannot seek\n");
-    else
-        printf("seek OK\n");
-	exit(0);
+    int     fd;
+
+    if ((fd = creat("file.hole", FILE_MODE)) < 0)
+        err_sys("creat error");
+
+    if (write(fd, buf1, 10) != 10)
+        err_sys("buf1 write error");
+    /* offset now = 10 */
+
+    if (lseek(fd, 16384, SEEK_SET) == -1)
+        err_sys("lseek error");
+    /* offset now = 16384 */
+
+    if (write(fd, buf2, 10) != 10)
+        err_sys("buf2 write error");
+    /* offset now = 16394 */
+
+    exit(0);
 }
+
