979c70b9fc092a8c84cf8c61ac0a2c7fcae80906
asklux <asklux@398d6864-e22c-11de-a155-4d945b6d75ec>
2010-01-25 08:02:57 +0000

    apue fig3.10

--- apue/test.c
+++ apue/test.c
@@ -1,29 +1,48 @@
 #include "apue.h"
 #include <fcntl.h>
-
-char    buf1[] = "abcdefghij";
-char    buf2[] = "ABCDEFGHIJ";
-
 int
-main(void)
+main(int argc, char *argv[])
 {
-    int     fd;
 
-    if ((fd = creat("file.hole", FILE_MODE)) < 0)
-        err_sys("creat error");
+    int       val;
 
-    if (write(fd, buf1, 10) != 10)
-        err_sys("buf1 write error");
-    /* offset now = 10 */
+    if (argc != 2)
+        err_quit("usage: a.out <descriptor#>");
 
-    if (lseek(fd, 16384, SEEK_SET) == -1)
-        err_sys("lseek error");
-    /* offset now = 16384 */
+    if ((val = fcntl(atoi(argv[1]), F_GETFL, 0)) < 0)
+        err_sys("fcntl error for fd %d", atoi(argv[1]));
 
-    if (write(fd, buf2, 10) != 10)
-        err_sys("buf2 write error");
-    /* offset now = 16394 */
+    switch (val & O_ACCMODE) {
+    case O_RDONLY:
+        printf("read only");
+        break;
 
+    case O_WRONLY:
+        printf("write only");
+        break;
+
+    case O_RDWR:
+        printf("read write");
+        break;
+
+    default:
+        err_dump("unknown access mode");
+    }
+
+    if (val & O_APPEND)
+        printf(", append");
+    if (val & O_NONBLOCK)
+        printf(", nonblocking");
+#if defined(O_SYNC)
+    if (val & O_SYNC)
+        printf(", synchronous writes");
+#endif
+#if !defined(_POSIX_C_SOURCE) && defined(O_FSYNC)
+    if (val & O_FSYNC)
+        printf(", synchronous writes");
+#endif
+    putchar('\n');
     exit(0);
 }
+
 
