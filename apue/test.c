892b18f1d75be155586aea2e4a60b1dda8ad9c88
asklux <asklux@398d6864-e22c-11de-a155-4d945b6d75ec>
2010-01-27 12:46:30 +0000

    apue sec4.19 utime()

--- apue/test.c
+++ apue/test.c
@@ -1,48 +1,31 @@
 #include "apue.h"
 #include <fcntl.h>
+#include <utime.h>
+
 int
 main(int argc, char *argv[])
 {
+    int            i, fd;
+    struct stat    statbuf;
+    struct utimbuf timebuf;
 
-    int       val;
-
-    if (argc != 2)
-        err_quit("usage: a.out <descriptor#>");
-
-    if ((val = fcntl(atoi(argv[1]), F_GETFL, 0)) < 0)
-        err_sys("fcntl error for fd %d", atoi(argv[1]));
-
-    switch (val & O_ACCMODE) {
-    case O_RDONLY:
-        printf("read only");
-        break;
-
-    case O_WRONLY:
-        printf("write only");
-        break;
-
-    case O_RDWR:
-        printf("read write");
-        break;
-
-    default:
-        err_dump("unknown access mode");
+    for(i = 1; i < argc; i++) {
+        if(stat(argv[i], &statbuf) < 0) {
+            err_ret("%s: stat error", argv[i]);
+            continue;
+        }
+        if((fd = open(argv[i], O_RDWR | O_TRUNC)) < 0) {
+            err_ret("%s: open error", argv[i]);
+            continue;
+        }
+        close(fd);
+        timebuf.actime = statbuf.st_atime;
+        timebuf.modtime = statbuf.st_mtime;
+        if(utime(argv[i], &timebuf) < 0) {
+            err_ret("%s: utime error", argv[i]);
+            continue;
+        }
     }
-
-    if (val & O_APPEND)
-        printf(", append");
-    if (val & O_NONBLOCK)
-        printf(", nonblocking");
-#if defined(O_SYNC)
-    if (val & O_SYNC)
-        printf(", synchronous writes");
-#endif
-#if !defined(_POSIX_C_SOURCE) && defined(O_FSYNC)
-    if (val & O_FSYNC)
-        printf(", synchronous writes");
-#endif
-    putchar('\n');
     exit(0);
 }
-
 
