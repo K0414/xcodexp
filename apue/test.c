b0d55ee82486e8b2627f8080c412f16adbd9f033
asklux <asklux@398d6864-e22c-11de-a155-4d945b6d75ec>
2010-01-30 15:32:51 +0000

    apue chap6

--- apue/test.c
+++ apue/test.c
@@ -1,31 +1,24 @@
 #include "apue.h"
-#include <fcntl.h>
-#include <utime.h>
+#include <time.h>
 
 int
-main(int argc, char *argv[])
+main(void)
 {
-    int            i, fd;
-    struct stat    statbuf;
-    struct utimbuf timebuf;
+    time_t      caltime;
 
-    for(i = 1; i < argc; i++) {
-        if(stat(argv[i], &statbuf) < 0) {
-            err_ret("%s: stat error", argv[i]);
-            continue;
-        }
-        if((fd = open(argv[i], O_RDWR | O_TRUNC)) < 0) {
-            err_ret("%s: open error", argv[i]);
-            continue;
-        }
-        close(fd);
-        timebuf.actime = statbuf.st_atime;
-        timebuf.modtime = statbuf.st_mtime;
-        if(utime(argv[i], &timebuf) < 0) {
-            err_ret("%s: utime error", argv[i]);
-            continue;
-        }
-    }
+    if ((caltime = time(NULL)) == -1)
+        err_sys("time error");
+
+    printf("%ld\n", caltime);
+
+	caltime &= 0xFFFFFFFF;
+
+	while(caltime) {
+		printf("%ld", caltime % 2);
+		caltime /= 2;
+	}
+	printf("\n");
+
     exit(0);
 }
 
