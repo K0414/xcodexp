ae17fe00cc3e7e5cdf2eb5eb55ba66de94389520
asklux <asklux@398d6864-e22c-11de-a155-4d945b6d75ec>
2010-02-01 14:44:25 +0000

    apue sec8.2 fork()

--- apue/test.c
+++ apue/test.c
@@ -1,17 +1,30 @@
 #include "apue.h"
 
+int     glob = 6;       /* external variable in initialized data */
+char    buf[] = "a write to stdout\n";
+
 int
 main(void)
 {
-	void  **ptr;
-	int    i;
+    int       var;      /* automatic variable on the stack */
+    pid_t     pid;
 
-	ptr = calloc(5, sizeof(void *));
+    var = 88;
+    if (write(STDOUT_FILENO, buf, sizeof(buf)-1) != sizeof(buf)-1)
+        err_sys("write error");
+    printf("before fork\n");    /* we don't flush stdout */
+/*	printf("before fork"); */
 
-	for(i = 0; i < 5; i++) {
-		printf("0x%x 0x%x\n", (unsigned int)(ptr + i), (unsigned int)*(ptr + i));
-	}
+    if ((pid = fork()) < 0) {
+        err_sys("fork error");
+    } else if (pid == 0) {      /* child */
+        glob++;                 /* modify variables */
+        var++;
+    } else {
+        sleep(2);               /* parent */
+    }
 
+    printf("pid = %d, glob = %d, var = %d\n", getpid(), glob, var);
     exit(0);
 }
 
