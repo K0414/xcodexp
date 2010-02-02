d03ca52b6f7845a974804a2d56eb360b60a0464c
asklux <asklux@398d6864-e22c-11de-a155-4d945b6d75ec>
2010-02-02 16:48:28 +0000

    apue

--- apue/test.c
+++ apue/test.c
@@ -1,36 +1,17 @@
 #include "apue.h"
-#include <sys/wait.h>
 
 int
-main(void)
+main(int argc, char *argv[])
 {
-    pid_t   pid;
+    int     status;
 
-    if ((pid = fork()) < 0) {
-        err_sys("fork error");
-    } else if (pid == 0) {     /* first child */
-        if ((pid = fork()) < 0)
-            err_sys("fork error");
-        else if (pid > 0)
-            exit(0);    /* parent from second fork == first child */
-        /*
-         * We're the second child; our parent becomes init as soon
-         * as our real parent calls exit() in the statement above.
-         * Here's where we'd continue executing, knowing that when
-         * we're done, init will reap our status.
-         */
-        sleep(2);
-        printf("second child, parent pid = %d\n", getppid());
-        exit(0);
-    }
-    
-    if (waitpid(pid, NULL, 0) != pid)  /* wait for first child */
-        err_sys("waitpid error");
+    if (argc < 2)
+        err_quit("command-line argument required");
 
-    /*
-     * We're the parent (the original process); we continue executing,
-     * knowing that we're not the parent of the second child.
-     */
+    if ((status = system(argv[1])) < 0)
+        err_sys("system() error");
+    pr_exit(status);
+
     exit(0);
 }
 
