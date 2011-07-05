e428b48b6649f400cf5fe4d23aa6e0ba4bf350ab
asklux <asklux@398d6864-e22c-11de-a155-4d945b6d75ec>
2011-07-05 02:47:16 +0000

    cpu frame done

--- verilog/MyPS/cpu.v
+++ verilog/MyPS/cpu.v
@@ -115,3 +115,176 @@ assign idbus[15:0]=idbus_stvalue;
 endmodule
 
 
+'timescale 1ns/10ps
+module alu_module(clock, reset, idbus, exbus, ex_dest);
+input clock;
+input reset;
+input [55:0] idbus;
+output [29:0] exbus;
+output [2:0] ex_dest;
+
+wire idbus_valid;
+wire [3:0] idbus_op;
+wire [2:0] idbus_dest;
+wire [15:0] idbus_value1;
+wire [15:0] idbus_value2;
+wire [15:0] idbus_stvalue;
+
+wire exbus_valid;
+wire [3:0]exbus_op;
+wire [2:0]exbus_dest;
+wire [15:0]exbus_exresult;
+wire [15:0]exbus_stvalue;
+
+assign idbus_valid=idbus[55];
+assign idbus_op=idbus[54:51];
+assign idbus_dest=idbus[50:48];
+assign idbus_value1=idbus[47:32];
+assign idbus_value2=idbus[31:16];
+assign idbus_stvalue=idbus[15:0];
+
+// alu_module transaction
+
+assign exbus[39]=exbus_valid;
+assign exbus[38:35]=exbus_op;
+assign exbus[34:32]=exbus_dest;
+assign exbus[31:16]=exbus_exresult;
+assign exbus[15:0]=exbus_stvalue;
+
+endmodule
+
+
+'timescale 1ns/10ps
+module mem_decode(clock, reset, exbus, membus, mem_dest);
+input clock;
+input reset;
+input [39:0] exbus;
+output [39:0] membus;
+output [2:0] mem_dest;
+
+wire exbus_valid;
+wire [3:0] exbus_op;
+wire [2:0] exbus_dest;
+wire [15:0] exbus_exresult;
+wire [15:0] exbus_stvalue;
+
+wire membus_valid;
+wire [3:0] membus_op;
+wire [2:0] membus_dest;
+wire [15:0] membus_exresult;
+wire [15:0] membus_memvalue;
+
+assign exbus_valid=exbus[39];
+assign exbus_op=exbus[38:35];
+assign exbus_dest=exbus[34:32];
+assign exbus_exresult=exbus[31:16];
+assign exbus_stvalue=exbus[15:0];
+
+// mem_module transaction
+
+assign membus[39]=membus_valid;
+assign membus[38:35]=membus_op;
+assign membus[34:32]=membus_dest;
+assign membus[31:16]=membus_exresult;
+assign membus[15:0]=membus_stvalue;
+
+endmodule
+
+
+'timescale 1ns/10ps
+module wb_module(clock, reset, membus, wbbus, wb_dest);
+input clock;
+input reset;
+input [39:0] membus;
+output [19:0] wbbus;
+output [2:0] wb_dest;
+
+wire membus_valid;
+wire [3:0] membus_op;
+wire [2:0] membus_dest;
+wire [15:0] membus_exresult;
+wire [15:0] membus_memresult;
+
+wire wbbus_valid;
+wire [2:0] wbbus_dest;
+wire [15:0] wbbus_value;
+
+assign membus_valid=membus[39];
+assign membus_op=membus[38:35];
+assign membus_dest=membus[34:32];
+assign membus_exresult=membus[31:16];
+assign membus_memresult=membus[15:0];
+
+// wb_module transaction
+
+assign wbbus[19]=wbbus_valid;
+assign wbbus[18:16]=wbbus_dest;
+assign wbbus[15:0]=wbbus_value;
+
+endmodule
+
+
+'timescale 1ns/10ps
+module rom(raddr, rout);
+input [11:0] raddr;
+output [15:0] rout;
+
+reg [15:0] rom[4095:0];
+integer i;
+initial
+begin
+		$readmemb("rom.vlog", rom);
+		$display("\nLoad rom successfully!! \n\n");
+end
+assign rout = rom[raddr];
+
+endmodule
+
+
+'timescale 1ns/10ps
+module ram(clock, raddr, rout, wen, waddr, win);
+input clock;
+input wen;
+input [15:0] win;
+input [11:0] raddr;
+input [11:0] waddr;
+output [15:0] rout;
+
+reg [15:0] ram[4095:0];
+assign rout = ram[raddr];
+
+always @(posedge clock)
+begin
+		if (wen)
+		begin
+				ram[waddr] = win;
+		end
+end
+
+endmodule
+
+
+'timescale 1ns/10ps
+module regfile(clock, raddr1, rout1, raddr2, rout2, wen, waddr, win);
+input clock;
+input wen;
+input [15:0] win;
+input [2:0] raddr1, raddr2;
+input [2:0] waddr;
+output [15:0] rout1, rout2;
+
+reg [15:0] ram[7:0];
+assign rout1 = ram[raddr1];
+assign rout2 = ram[raddr2];
+
+always @(posedge clock)
+begin
+		ram[0] <= 16'b0;
+		if (wen)
+		begin
+				if (waddr != 0)
+						ram[waddr] <= win;
+		end
+end
+
+endmodule
