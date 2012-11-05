a2231430e087fe214ef311f2577e0155119bf8c2
K0414 <liufc10@gmail.com>
2012-11-05 23:44:36 +0800

    add knapsack

---
+++ algorithm/knapsack/knapsack.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+static const int items[] = {53, 74, 72, 75, 55, 50, 83, 23, 98, 61, 42, 16, 10, 21, 52, 54, 67, 92, 34};
+
+int knapsack_01()
+{
+
+}
+
+int main()
+{
+    return 0;
+}
---
+++ algorithm/knapsack/rand.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import random
+
+def main():
+    for i in range(1, 20):
+        print random.randint(10, 99)
+
+if __name__ == "__main__":
+    main()
