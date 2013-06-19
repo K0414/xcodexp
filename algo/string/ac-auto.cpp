266be52b2ee948c379518789b88ed392711c31a3
K0414 <liufc10@gmail.com>
2013-06-19 17:58:54 +0800

    ac-auto temp

--- algo/string/ac-auto.cpp
+++ algo/string/ac-auto.cpp
@@ -8,24 +8,40 @@ using namespace std;
 class TNode {
 public:
     static const int n_slots = 4;
-    TNode* cn[n_slots];
-    char c;
-    TNode *next;
-    bool isend;
 
     TNode(char c)
     {
         this->c = c;
         memset(this->cn, 0, n_slots * sizeof(TNode*));
-        this->next = NULL;
+        this->next = this->parent = this->fail = NULL;
         this->isend = false;
     }
-    ~TNode() {}
+    ~TNode()
+    {
+        TNode *p, *t;
+        for(int i=0; i<n_slots; i++) {
+            if(cn[i]) {
+                p = cn[i];
+                while(p) {
+                    t = p;
+                    p = p->next;
+                    delete t;
+                }
+            }
+        }
+    }
     void add_pattern(string s);
-    bool match(string s);
+    void make_fail_links();
+    bool match(string s, vector<int>& res);
 private:
-    void insert(string s);
     TNode *get_child(char c);
+    queue<TNode*> q_nodes;
+    TNode* cn[n_slots];
+    char c;
+    TNode *next;
+    TNode *parent;
+    TNode *fail;
+    bool isend;
 };
 
 TNode *TNode::get_child(char c)
@@ -40,12 +56,13 @@ TNode *TNode::get_child(char c)
     }
 }
 
-void TNode::insert(string s)
+void TNode::add_pattern(string s)
 {
     if(s.size() <= 0) return;
     TNode *p = get_child(s[0]);
     if(p == NULL) {
         TNode *nn = new TNode(s[0]);
+        nn->parent = this;
         int i = s[0] % n_slots;
         if(cn[i] == NULL) {
             cn[i] = nn;
@@ -59,29 +76,70 @@ void TNode::insert(string s)
     }
     
     if(s.size() > 1)
-        p->insert(s.substr(1));
+        p->add_pattern(s.substr(1));
     else
         p->isend = true;
 }
 
+void TNode::make_fail_links()
+{
+    /* pn <-> pnode, pp <-> pparent, pl <-> plink(fail) */
+    TNode *p, *pn, *pp, *pl;
+    this->fail = this;
+    q_nodes.push(this);
+    while(!q_nodes.empty()) {
+        pn = q_nodes.front();
+        q_nodes.pop();
+        for(int i=0; i<n_slots; i++) {
+            TNode *ps = cn[i];
+            while(ps) {
+                pp = ps->parent;
+                if(pp && pp != this) {
+                    pl = pp->fail;
+                    while(pl->get_child(pn->c) == NULL && pl != this) {
+                        pl = pl->fail;
+                    }
+                    p = pl->get_child(pn->c);
+                    if(p) {
+                        pn->fail = p;
+                    } else {
+                        pn->fail = this;
+                    }
+                }
+                q_nodes.push(ps);
+                ps = ps->next;
+            }
+        }
+    }
+}
 
+
+bool TNode::match(string s, vector<int>& res)
+{
+    return ;
+}		/* -----  end of function TNode::match  ----- */
 
 int main()
 {
     string s, p;
+    vector<int> res;
     while(true) {
-        TNode trie('^');
-        cout << "insert: ";
+        TNode acauto('^');
+        cout << "add pattern: ";
         cin >> s;
         while(s != "#") {
-            trie.insert(s);
-            cout << "insert: ";
+            acauto.add_pattern(s);
+            cout << "add pattern: ";
             cin >> s;
         }
         cout << "search: ";
         cin >> p;
         while(p != "#") {
-            cout << (trie.contains(p) ? "True" : "False") << endl;
+            if(acauto.match(p, res)) {
+
+            } else {
+                cout << "Not Found." << endl;
+            }
             cout << "search: ";
             cin >> p;
         }
