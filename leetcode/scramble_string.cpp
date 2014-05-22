b5a8a2b2029d4ee4e6ef30a561560b8ef9bd171d
K0414 <liufc10@gmail.com>
2014-05-22 19:32:01 +0800

    leetcode update.

---
+++ leetcode/scramble_string.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool isScramble(string s1, string s2) {
+        
+    }
+};
+
+
+bool test()
+{}
+
+int main()
+{
+    return 0;
+}
