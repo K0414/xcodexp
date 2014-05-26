b8418f711e2bd0aef1472719cfafd6f9d5b316e2
K0414 <liufc10@gmail.com>
2014-05-26 23:58:06 +0800

    leetcode update.

---
+++ leetcode/permutations_ii.py
@@ -0,0 +1,16 @@
+from nose.tools import assert_equal
+
+class Solution:
+    # @param num, a list of integer
+    # @return a list of lists of integers
+    def permuteUnique(self, num):
+        pass
+
+
+class TestSolution(object):
+
+    def test_simple(self):
+        s = Solution()
+
+    def test_example(self):
+        s = Solution()
