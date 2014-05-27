7f83fc608c26a51f4f3f32beec091a11658adfc8
K0414 <liufc10@gmail.com>
2014-05-27 17:47:15 +0800

    leetcode update.

---
+++ leetcode/reverse_nodes_in_k_group.py
@@ -0,0 +1,23 @@
+from nose.tools import assert_equal
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    # @param head, a ListNode
+    # @param k, an integer
+    # @return a ListNode
+    def reverseKGroup(self, head, k):
+        pass
+
+
+class TestSolution(object):
+
+    def test_simple(self):
+        s = Solution()
+
+    def test_example(self):
+        s = Solution()
