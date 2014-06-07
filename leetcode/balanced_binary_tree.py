4a7754ce2f8556857785090ae3e64757d9c87204
K0414 <liufc10@gmail.com>
2014-06-08 00:39:39 +0800

    Add balanced_binary_tree skeleton.

---
+++ leetcode/balanced_binary_tree.py
@@ -0,0 +1,23 @@
+from nose.tools import assert_equal
+
+# Definition for a  binary tree node
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    # @param root, a tree node
+    # @return a boolean
+    def isBalanced(self, root):
+        pass
+
+
+class TestSolution(object):
+
+    def test_simple(self):
+        s = Solution()
+
+    def test_example(self):
+        s = Solution()
